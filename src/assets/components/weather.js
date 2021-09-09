7368d786b606dde59fec7c38fcbf8ab9133519f4
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-09-09 11:35:09 +0300

    polished style

--- src/assets/components/weather.js
+++ src/assets/components/weather.js
@@ -1,20 +1,33 @@
 import React from "react";
 import WeatherDay from "./weatherDay";
+import { Table } from "react-bootstrap";
 
 class Weather extends React.Component {
   render() {
     if (this.props.data) {
       return (
         <>
-          <div>
-              {this.props.data.map(item=>{
-                  return (<WeatherDay date={item.date} desc={item.description}/>)
-              })}
+          <div id="tableContainer">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Date:</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.data.map((item) => {
+                  return (
+                    <WeatherDay date={item.date} desc={item.description} />
+                  );
+                })}
+              </tbody>
+            </Table>
           </div>
         </>
       );
-    }
-    else {
+    } else {
       return <></>;
     }
   }
